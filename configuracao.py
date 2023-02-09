56832f99c21d1a6ec6a9ec6192520eb6390482a4
Danilo Soares <danilo.soares@dce.ufpb.br>
2023-02-09 19:39:50 -0300

    Atividades do Módulo 1

---
+++ configuracao.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+if(True):
+    print("Verdadeiro")
+elif(False):
+    print("Falso")
+else:
+    print("Falso")
+    
+print("Teste")
+
+print("Teste")
---
+++ teste.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+print("Verdadeiro")
+print("Falso")
+
+if(True):
+    print("Verdadeiro")
+    
+print("Teste")
